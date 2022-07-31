8ab82afab9e2819a71ecbe0baac4d3ed68ab53c6
laixintao <laixintaoo@gmail.com>
2022-08-01 00:44:14 +0800

    make it work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cffi
 import os
+import json
 
 ffi = cffi.FFI()
 
@@ -9,13 +10,22 @@ lib = ffi.dlopen("./libpromql.so")
 
 # Define the function prototypes
 ffi.cdef('''
-    typedef struct { char* json_result; char* err; } Result;
-    Result* split(char* code);
+
+struct split_return {
+	char* json_result;
+	char* err;
+};
+
+extern struct split_return split(char* code);
 ''')
 
 
+print(dir(lib))
 result = lib.split('sum(rate(foo{bar="baz"}[5m])) by (job) > 0'.encode())
-p=ffi.unpack(ffi.cast("Result*", result), 1)
-p1 = ffi.string(p.json_result).decode()
-print("StringFromGo(): {}".format(p))
-print("StringFromGo(): {}".format(p1))
+p1 = ffi.string(result.json_result).decode()
+p2 =  ffi.string(result.err).decode()
+
+print(json.loads(p1))
+print(p2)
+import time
+time.sleep(111111)
