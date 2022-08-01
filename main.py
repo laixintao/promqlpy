b33ac4f7781e255c678262f2776c8fce756d688b
laixintao <laixintaoo@gmail.com>
2022-08-01 11:40:18 +0800

    add test cases.

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-import cffi
-import os
-import json
+from promqlpy import split
 
-ffi = cffi.FFI()
+a = split('sum(rate(foo{bar="baz"}[5m])) by (job) > 0')
+print(a)
 
-# Load library
-lib = ffi.dlopen("./libpromql.so")
-
-
-# Define the function prototypes
-ffi.cdef('''
-
-struct split_return {
-	char* json_result;
-	char* err;
-};
-
-extern struct split_return split(char* code);
-''')
-
-
-print(dir(lib))
-result = lib.split('sum(rate(foo{bar="baz"}[5m])) by (job) > 0'.encode())
-p1 = ffi.string(result.json_result).decode()
-p2 =  ffi.string(result.err).decode()
-
-print(json.loads(p1))
-print(p2)
-import time
-time.sleep(111111)
+count = 0
+while 1:
+    with open("tests/data/awesome_prometheus_alerts.txt", "r") as rulesfile:
+        for line in rulesfile:
+            count += 1
+            split(line)
+            print(count)
